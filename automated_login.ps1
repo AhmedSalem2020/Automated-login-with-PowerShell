f610e1c1d726c84fce67094ae5370a7168c501fd
Ahmed Salem <ahmed.m.salem2020@outlook.com>
2020-03-14 18:57:10 +0200

    Update automated_login.ps1

--- automated_login.ps1
+++ automated_login.ps1
@@ -11,8 +11,8 @@ if ($Null -eq $ie) {
 $ie.Navigate("http://facebook.com/")
 
 #Navigate2() function to open in new tab in the same IE window
-$ie.Navigate2("http://outlook.com",0x1000)  
 $ie.Navigate2("http://https://github.com/",0x1000) 
+#$ie.Navigate2("http://outlook.com",0x1000) 
 #$ie.Navigate2("https://www.linkedin.com/",0x1000) 
 
 # Script to wait till webpage is downloaded into the browsers.
@@ -26,28 +26,27 @@ $ie = (New-Object -COM "Shell.Application").Windows() |Where-Object{$_.locationn
 If($null -eq $ie){ $ie.Refresh()}
 while ($ie.Busy -eq $true){Start-Sleep -seconds 3}
 if ($Null -eq $ie.Document) { $ie.Document }
-$ie.document.getElementById("ok").click()
 # Feed in your credentials to input fields on the web page
-$usernamefield = $ie.Document.getElementById("j_username")
+$usernamefield = $ie.Document.getElementById("email")
 $usernamefield.value = "mail"
-$passwordfield = $ie.document.getElementById("password")
+$passwordfield = $ie.document.getElementById("pass")
 $passwordfield.value = "pass"
-$Link=$ie.Document.getElementsByClassName("btn-primary loginButton") | Select-Object -First 1
+$Link=$ie.Document.getElementById("loginbutton") | Select-Object -First 1
 $Link.click()
 
 # Outlook Login
 # To Identify the Outlook window among the existing IE tabs.
-$ie = (New-Object -COM "Shell.Application").Windows() | Where-Object{$_.locationname -like '*Outlook*'}
-If($null -eq $ie.Document){ $ie.Refresh()}
-while ($ie.Busy -eq $true){Start-Sleep -seconds 1; Write-Output 'loading outlook  tab...'}
+#$ie = (New-Object -COM "Shell.Application").Windows() | Where-Object{$_.locationname -like '*Outlook*'}
+#If($null -eq $ie.Document){ $ie.Refresh()}
+#while ($ie.Busy -eq $true){Start-Sleep -seconds 1; Write-Output 'loading outlook  tab...'}
 
 # Feed in your credentials to input fields on the web page
-$usernamefield = $ie.Document.getElementByID("userloginValue")
-$usernamefield.value = "mail"
-$passwordfield = $ie.Document.getElementByID("userpassword")
-$passwordfield.value = "pass"
-$Link=$ie.Document.getElementsByClassName("btn-primary loginButton")
-$Link.click()
+#$usernamefield = $ie.Document.getElementsByClassName("input__field input__field--with-label")
+#$usernamefield.value = "mail"
+#$passwordfield = $ie.Document.getElementByID("userpassword")
+#$passwordfield.value = "pass"
+#$Link=$ie.Document.getElementsByClassName("btn-primary loginButton")
+#$Link.click()
 
 # GitHub Login
 # To Identify the GitHub window among the existing IE tabs.
@@ -56,9 +55,9 @@ If($null -eq $ie.Document){ $ie.Refresh()}
 while ($ie.Busy -eq $true){Start-Sleep -seconds 1; Write-Output 'loading Live.com '}
 
 # Feed in your credentials to input fields on the web page
-$usernamefield = $ie.Document.getElementByID('i0116')
+$usernamefield = $ie.Document.getElementByID('login_field')
 $usernamefield.value = 'mail'
-$passwordfield = $ie.Document.getElementByID('i0118')
+$passwordfield = $ie.Document.getElementByID('password')
 $passwordfield.value = 'pass'
-$Link=$ie.Document.getElementByID('idSIButton9')
+$Link=$ie.Document.getElementsByClassName('btn btn-primary btn-block')
 $Link.click() 
